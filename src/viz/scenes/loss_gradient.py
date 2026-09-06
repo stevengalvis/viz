86d9fa77b9cf282267b1c9af60c2657abdf1fabf
stevengalvis <steven.galvis@gmail.com>
2026-09-06 11:02:19 -0400

    Simplify loss gradient teaching preview

--- src/viz/scenes/loss_gradient.py
+++ src/viz/scenes/loss_gradient.py
@@ -8,19 +8,17 @@ from manim import (
     GREEN,
     RED,
     RIGHT,
-    UL,
     UP,
-    WHITE,
     YELLOW,
     Arrow,
     Axes,
     Create,
-    DashedLine,
     Dot,
     FadeIn,
     FadeOut,
     MathTex,
     Scene,
+    Text,
     Write,
 )
 
@@ -51,13 +49,11 @@ class LossGradientScene(Scene):
             x_length=7,
             y_length=4.5,
             axis_config={"include_tip": False},
-        ).add_coordinates()
-        labels = axes.get_axis_labels(MathTex("w"), MathTex("L(w)"))
+        )
         curve = axes.plot(loss, x_range=[-2.4, 2.4], color=BLUE)
-        equation = MathTex(r"L(w)=w^2", color=BLUE).to_corner(UL)
+        equation = MathTex(r"L(w)=w^2", color=BLUE).next_to(axes, UP)
 
-        self.play(Create(axes), Write(labels), run_time=1.2)
-        self.play(Create(curve), Write(equation), run_time=1.2)
+        self.play(Create(axes), Create(curve), Write(equation), run_time=1.3)
 
         start = self.START_WEIGHT
         gradient = 2 * start
@@ -66,42 +62,39 @@ class LossGradientScene(Scene):
         end_point = axes.c2p(next_weight, loss(next_weight))
 
         dot = Dot(start_point, color=YELLOW)
-        guide = DashedLine(axes.c2p(start, 0), start_point, color=WHITE)
-        weight_label = MathTex(r"w_0", color=YELLOW).next_to(dot)
-        self.play(Create(guide), FadeIn(dot), Write(weight_label), run_time=0.8)
+        weight_label = Text("current weight", font_size=28, color=YELLOW).next_to(
+            dot, UP
+        )
+        self.play(FadeIn(dot), Write(weight_label), run_time=0.8)
 
         tangent = axes.plot(
             tangent_function(start), x_range=[0.9, 2.35], color=RED
         )
-        slope_label = MathTex(r"\nabla L(w_0)=2w_0>0", color=RED).to_edge(UP)
+        slope_label = Text("slope", font_size=28, color=RED).next_to(
+            tangent, RIGHT
+        )
         self.play(Create(tangent), Write(slope_label), run_time=1.0)
 
         direction_arrow = Arrow(
-            axes.c2p(start, 0.45),
-            axes.c2p(next_weight, 0.45),
-            buff=0,
+            start_point + DOWN * 0.4,
+            end_point + UP * 0.25,
+            buff=0.1,
             color=GREEN,
         )
-        direction_label = MathTex(r"-\nabla L", color=GREEN).next_to(
-            direction_arrow, direction=DOWN
+        direction_label = Text("move downhill", font_size=28, color=GREEN).next_to(
+            direction_arrow, DOWN
         )
-        self.play(Create(direction_arrow), Write(direction_label), run_time=0.8)
+        self.play(Create(direction_arrow), Write(direction_label), run_time=0.9)
 
-        end_guide = DashedLine(axes.c2p(next_weight, 0), end_point, color=WHITE)
-        update = MathTex(
-            r"w_1=w_0-\eta\nabla L(w_0)", color=GREEN
-        ).to_edge(UP)
         self.play(
             dot.animate.move_to(end_point),
-            FadeOut(guide),
             FadeOut(weight_label),
-            FadeIn(end_guide),
-            slope_label.animate.become(update),
+            FadeOut(tangent),
+            FadeOut(slope_label),
+            FadeOut(direction_arrow),
+            FadeOut(direction_label),
             run_time=1.3,
         )
-        new_label = MathTex(r"w_1", color=YELLOW).next_to(dot)
-        lower_loss = MathTex(r"L(w_1)<L(w_0)", color=GREEN).next_to(
-            new_label, direction=RIGHT
-        )
-        self.play(Write(new_label), Write(lower_loss), run_time=0.8)
-        self.wait(0.5)
+        lower_loss = Text("lower loss", font_size=32, color=GREEN).next_to(dot, RIGHT)
+        self.play(Write(lower_loss), run_time=0.8)
+        self.wait(0.6)
